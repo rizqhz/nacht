28a9492dd25aa4c6b258584027fec93de589cc42
Akasha d'Algazel <rizqhz.dev@gmail.com>
2024-03-12 20:06:03 +0700

    init nix flake setup

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Nacht • Akasha Nix Flake";
+
+  inputs = rec {
+    nixpkgs.url = "flake:nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, ... }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system} = {
+      backend = pkgs.mkShellNoCC rec {
+        packages = with pkgs; [
+          go_1_22
+        ];
+
+        GOPATH  = ~/w/go;
+        GOCACHE = ~/w/go/cache;
+        GOENV   = ~/w/go/env;
+      };
+
+      llvm = pkgs.mkShellNoCC rec {
+        packages = with pkgs.llvmPackages; [
+          clangUseLLVM
+        ];
+      };
+    };
+  };
+}
